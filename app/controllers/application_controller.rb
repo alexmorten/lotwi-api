a6c2328d09b3eb1ecba07b977e59b3be718d8b0d
alexmorten <alexandrius.martinus@gmx.de>
2016-10-21 22:19:51 +0200

    first talking version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+  after_action :cors_set_access_control_headers
+
+def cors_set_access_control_headers
+  headers['Access-Control-Allow-Origin']      = '*'
+  headers['Access-Control-Allow-Methods']     = 'POST, GET, OPTIONS'
+  headers['Access-Control-Max-Age']           = '1728000'
+  headers['Access-Control-Allow-Credentials'] = 'true'
+end
 end
--- config/initializers/json_api.rb
+++ config/initializers/json_api.rb
@@ -1,3 +1,2 @@
 require 'active_model_serializers/register_jsonapi_renderer'
-
-ActiveModelSerializers.config.adapter = :json_api
+ActiveModel::Serializer.config.adapter = :json
