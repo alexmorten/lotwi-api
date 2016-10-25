5d8c2409f11e104aded33d6f4af690f4c0d92f0c
alexmorten <alexandrius.martinus@gmx.de>
2016-10-25 23:37:38 +0200

    added created to posts serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :text,:dist
+  attributes :id, :title, :text,:dist,:created
   has_one :location
   def dist
     lat = instance_options[:lat]
@@ -9,5 +9,9 @@ class PostSerializer < ActiveModel::Serializer
     else
       return nil
     end
+  end
+
+  def created
+    object.created_at
   end
 end
